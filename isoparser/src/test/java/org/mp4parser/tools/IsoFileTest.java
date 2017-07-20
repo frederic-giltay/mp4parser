dd953a62e5001cde947e6774c378eec0232f8879
Frederic Giltay <frederic.giltay@synchronoss.com>
2017-07-20 09:18:22 +0200

    Parsing of boxes with size 0

--- isoparser/src/test/java/org/mp4parser/tools/IsoFileTest.java
+++ isoparser/src/test/java/org/mp4parser/tools/IsoFileTest.java
@@ -1,12 +1,18 @@
 package org.mp4parser.tools;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.mp4parser.IsoFile;
+import org.mp4parser.boxes.apple.AppleGPSCoordinatesBox;
+import org.mp4parser.boxes.iso14496.part12.MovieBox;
+import org.mp4parser.boxes.iso14496.part12.UserDataBox;
+import org.mp4parser.boxes.threegpp.ts26244.LocationInformationBox;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.channels.Channels;
+import java.util.List;
 
 /**
  *
@@ -27,6 +33,30 @@ public class IsoFileTest extends TestCase {
     public void testZeroSizeBox() throws Exception {
         File f = new File(IsoFileTest.class.getResource("/zero-size-mov.mov").toURI());
         IsoFile isoFile = new IsoFile(Channels.newChannel(new FileInputStream(f)), f.length());
+        String gps = extractGPSCoordinates(isoFile.getMovieBox());
+        assertNotNull(gps);
+    }
 
+    private String extractGPSCoordinates(MovieBox movieBox) {
+        List<UserDataBox> metaBoxes = movieBox.getBoxes(UserDataBox.class);
+
+        if (!metaBoxes.isEmpty()) {
+            UserDataBox userDataBox = metaBoxes.get(0);
+
+            List<AppleGPSCoordinatesBox> appleGPSCoordinatesBoxes = userDataBox.getBoxes(AppleGPSCoordinatesBox.class);
+
+            if (!appleGPSCoordinatesBoxes.isEmpty()) {
+                AppleGPSCoordinatesBox appleGPSCoordinatesBox = appleGPSCoordinatesBoxes.get(0);
+                return appleGPSCoordinatesBox.getValue();
+            }
+            List<LocationInformationBox> locationInformationBoxes = userDataBox.getBoxes(LocationInformationBox.class);
+            if (!locationInformationBoxes.isEmpty()) {
+                LocationInformationBox locationInformationBox = locationInformationBoxes.get(0);
+
+                return ""+locationInformationBox.getLatitude()+locationInformationBox.getLongitude();
+            }
+        }
+
+        return null;
     }
 }
